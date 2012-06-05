9ca3af4543a8566a1988db027b40ed3484adaa20
Justin T Kim <tjk.abdn@gmail.com>
2012-06-05 16:14:01 +0100

    Finish user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+	#returns the Gravatar for a given user
+	def gravatar_for(user)
+		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+		gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}"
+		image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    end
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,4 +34,13 @@ SampleApp::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+
+  #speed up tests by lowering BCrypts cost function
+  require 'bcrypt'
+  silence_warnings do
+    BCrypt::Engine::DEFAULT_COST = BCrypt::Engine::MIN_COST
+  end
+
+
 end
