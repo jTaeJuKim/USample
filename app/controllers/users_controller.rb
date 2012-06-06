85bf1368ae390a5e8eb4ebc2e012579f88ecbfb1
Justin T Kim <tjk.abdn@gmail.com>
2012-06-06 11:08:16 +0100

    Chapter 7 Exercises Complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   	@user = User.new(params[:user])
   	if @user.save
   		#handle a successful save
-  		#flash[:success] = "Welcome to the Sample App!"
+  		flash[:success] = "Welcome to the Sample App!"
   		redirect_to @user
   	else
   		render 'new'
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -28,6 +28,13 @@ describe "UserPages" do
         it "should not create a user" do
           expect{click_button submit }.not_to change(User, :count)
         end
+
+        describe "after submission" do
+          before{click_button submit}
+
+          it{should have_selector('title', text: 'Sign up')}
+          it{should have_content('error')}
+        end
       end
 
       describe "with valid information" do
@@ -38,10 +45,23 @@ describe "UserPages" do
           fill_in "Confirmation", with: "foobar"
         end
 
+        describe "after creating the user" do
+          before{click_button submit}
+          let(:user){User.find_by_email('user@example.com')}
+          it{should have_selector('title', text: user.name)}
+          it{should have_selector('div.alert.alert-success', text: 'Welcome')}
+        end
+
+
+
         it "should create a user" do
           expect{click_button submit}.to change(User, :count).by(1)
         end
+
+        
       end
+
+
     end
 
 
